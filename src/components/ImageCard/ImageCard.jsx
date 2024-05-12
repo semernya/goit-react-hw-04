51ff549b22aa55b638cbbb55071ec0fb95e699bd
Anastasiia Semernya <semernya38@gmail.com>
2024-05-12 23:30:58 +0300

    modal

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,12 +3,12 @@ import css from './ImageCard.module.css'
 export default function ImageCard({ onClick, item: {
     alt_description,
     likes,
-    urls: { small },
+    urls: { small, regular },
     user: { instagram_username },
 } }) {
     return (
         <div className={css.imgBox}>
-            <img onClick={onClick} src={small} alt={alt_description} width='300' height='230' className={css.img} />
+            <img onClick={() => onClick(regular, likes, instagram_username)} src={small} alt={alt_description} width='300' height='230' className={css.img} />
             <div className={css.socials}>
                 <p>Likes: {likes}</p>
                 <p>{instagram_username}</p>
